6f652659719473619ece3888b0255860ff92ab6d
Adham <admuaq@gmail.com>
2018-09-03 13:10:59 +0100

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,32 @@ const codes = [
   "a"
 ];
 
-function init() {
-  // your code here
-}
+const init = () => {
+
+let index = 0
+
+const main = document.body 
+
+main.addEventListener('keydown', (event) => { 
+
+
+  for (key in codes) {
+      const key = event.key
+
+      if (key === codes[index]) {
+        index++
+
+        if (index === codes.length) {
+          index = 0;
+          return alert("Cheat code activated!")
+        }
+        return console.log("correct")
+      }
+
+      else {
+        index = 0
+        return console.log("incorrect")
+      }
+    }
+    })
+  }
